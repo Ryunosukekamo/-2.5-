02541a10e10b644ac56adfb38496cbdb88f155ab
加茂竜之介 <Kamoryu2001@gmail.com>
2021-12-18 20:36:43 +0900

    css

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function topview(){
+    let top_view = document.getElementById('top_view');
+    top_view.style.display="none";
+
+    let detailed_page = document.getElementById('detailed_page');
+    detailed_page.style.display = "block";
+}
+
+
